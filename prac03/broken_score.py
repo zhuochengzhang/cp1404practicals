1ae085e78fc54394c3ab759a6bf4112b6d6f4168
Zhuocheng Zhang <zhuochengz3@gmail.com>
2020-12-03 01:32:33 +0800

    Refactor to create functions to return the result string of score

--- prac03/broken_score.py
+++ prac03/broken_score.py
@@ -3,17 +3,28 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 # TODO: Fix this!
 
-score = float(input("Enter score: "))
-if score < 0:
-    print("Invalid score")
-else:
-    if score > 100:
-        print("Invalid score")
-    if score > 50:
-        print("Passable")
-    if score > 90:
-        print("Excellent")
-if score < 50:
-    print("Bad")
+def main():
+    score = float(input("Enter score: "))
+    result = score_result(score)
+    print(result)
+
+
+def score_result(score):
+    if score < 0:
+        return "Invalid score"
+    else:
+        if score > 100:
+            return "Invalid score"
+        if score > 50:
+            return "Passable"
+        if score > 90:
+            return "Excellent"
+    if score < 50:
+        return "Bad"
+
+
+if __name__ == '__main__':
+    main()
